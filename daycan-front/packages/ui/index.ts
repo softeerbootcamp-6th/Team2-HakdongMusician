15657c896437d0b3008dc21c78f5f520b026d08a
홍규진 <hkj0206@dgu.ac.kr>
2025-07-28 18:12:13 +0900

    chore: 각 폴더 별 목적을 파악하기 위한 주석 추가

--- daycan-front/packages/ui/index.ts
+++ daycan-front/packages/ui/index.ts
@@ -1,2 +1,9 @@
-// components
+/**
+ * 각 컴포넌트 파일에서 내보내는 것들을 모아서 한 번에 내보내는 파일
+ * 내부적으로 Components, Styles, Utils, Types 폴더 내부에 있는 파일들을 내보내는 파일입니다.
+ * @author 홍규진
+ */
 export * from "./src/components";
+export * from "./src/styles";
+export * from "./src/utils";
+export * from "./src/types";
--- daycan-front/packages/ui/src/components/index.ts
+++ daycan-front/packages/ui/src/components/index.ts
@@ -1,2 +1,9 @@
-export * from "./header";
-export * from "./counter";
+/**
+ * 디자인 시스템으로 정의되어있는 컴포넌트들을 내보내는 파일입니다.
+ * 내부적으로 Display, Heading, Body 폴더 내부에 있는 파일들을 내보내는 파일입니다.
+ * 이는 최상위 index.ts 에서 한 번 더 내보내기 위해 사용됩니다.
+ * @author 홍규진
+ */
+export * from "./Display";
+export * from "./Heading";
+export * from "./Body";
