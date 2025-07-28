9d4fc492a4bc7805982034ad347275f15a49e71e
kds <mekind98@gmail.com>
2025-07-28 17:01:52 +0900

    feat: spring boot init

---
+++ daycan-back/src/test/java/com/daycan/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.com.daycan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
