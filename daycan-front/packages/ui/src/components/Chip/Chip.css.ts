3a3fa787107654eba547a101298463969e23c67d
Bogil So <sotomas232@gmail.com>
2025-07-31 18:43:08 +0900

    Feature/fe/#44 Chip 컴포넌트 && Segment 컴포넌트 리팩토링 (#45)
    * delete: 컴포넌트 테스트 파일 삭제
    
    * refactor: segment 컴포넌트 구조 정리와 리팩토링
    
    * refactor: Chip컴포넌트 varients값 축소

--- daycan-front/packages/ui/src/components/Chip/Chip.css.ts
+++ daycan-front/packages/ui/src/components/Chip/Chip.css.ts
@@ -10,7 +10,6 @@ export const chip = recipe({
     gap: '4px',
     fontSize: '13px',
     fontWeight: 500,
-    borderRadius: '8px',
     color: COLORS.gray[100],
     backgroundColor: COLORS.gray[600],
     fontStyle: 'normal',
@@ -20,76 +19,6 @@ export const chip = recipe({
   },
   variants: {
     // 크기 variant
-    padding: {
-      btnDefaultPadding: {
-        padding: '6px 16px',
-        borderRadius: '8px',
-      },
-      btnXsmall: {
-        padding: '4px 10px',
-        borderRadius: '4px',
-      },
-      btnFilterSmall: {
-        padding: '6px 16px',
-        borderRadius: '8px',
-      },
-      btnFilterMedium: {
-        padding: '4px 10px',
-        borderRadius: '8px',
-      },
-      btnFilterLarge: {
-        padding: '12px 24px',
-        borderRadius: '13.5px',
-      },
-      small: {
-        padding: '2px 6px',
-        borderRadius: '4px',
-      },
-      medium:{
-        padding: '4px 8px',
-        borderRadius: '4px',
-      },
-      mediumLong:{
-        padding: '4px 16px',
-        borderRadius: '4px',
-      },
-      reportstatus:{
-        padding: '4px 16px',
-        borderRadius: '4px',
-      }
-    },
-    size:{
-        btnDefault: {
-            width:'61px',
-            height:'32px',
-        },
-        btnXsmall:{
-            width:'64px',
-            height:'28px',
-        },
-        filterSmall:{
-            width:'73px',
-            height:'32px',
-        },
-        filterMedium:{
-            width:'71px',
-            height:'31px',
-        },
-        filterLarge:{
-            width:'240px',
-            height:'56px',
-        },
-        small:{
-            width:'41px',
-            height:'24px',
-        },
-        medium:{
-            width:'49x',
-            height:'31px',
-        },
-        mediumLong:{
-        },
-    },
     flexRule:{
         spaceBetween: {
             justifyContent: 'space-between',
@@ -100,21 +29,24 @@ export const chip = recipe({
         },
         none:{}
     },
-    selected: {
-      true: {
-        backgroundColor: COLORS.gray[600],
-        color: COLORS.gray[100],
-      },
-      false: {
-        backgroundColor: COLORS.gray[50],
-        color: COLORS.gray[500],
-      },
-    },
+    round:{
+        default:{
+            borderRadius: '0px',
+        },
+        s:{
+            borderRadius: '4px',
+        },
+        m:{
+            borderRadius: '8px',
+        },
+        l:{
+            borderRadius: '13.5px',
+        },
+    }
   },
   defaultVariants: {
-    size: 'btnDefault',
-    padding: 'btnDefaultPadding',
     flexRule: 'none',
+    round: 'default',
   }
 });
 
--- daycan-front/packages/ui/src/components/Segment/SegmentItem/SegmentItem.tsx
+++ daycan-front/packages/ui/src/components/Segment/SegmentItem/SegmentItem.tsx
@@ -5,8 +5,6 @@ import { segmentItem, type SegmentItemVariants } from './SegmentItem.css';
 export type SegmentItemProps = HTMLAttributes<HTMLDivElement> & SegmentItemVariants;
 
 /**
- * SegmentItem 컴포넌트는 Segment의 각 아이템을 나타냅니다.
- * Chip컴포넌트와 다르게 SegmentItem은 ItemSegment내에서만 사용하므로 해당 위치에 작성합니다.
  * @param selected - 현재 선택된 상태
  * @returns SegmentItem 컴포넌트
  */
@@ -14,12 +12,14 @@ export type SegmentItemProps = HTMLAttributes<HTMLDivElement> & SegmentItemVaria
 export const SegmentItem = ({
   children,
   selected,
+  type,
+  flexRule,
   onClick,
   ...props
 }: SegmentItemProps) => {
   return (
     <div
-      className={classNames(segmentItem({ selected }))}
+      className={classNames(segmentItem({ selected, type, flexRule }))}
       onClick={onClick}
       {...props}
     >
