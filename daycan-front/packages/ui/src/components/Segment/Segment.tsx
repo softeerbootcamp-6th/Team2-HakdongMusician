0ce4c0437e7411bf0f413b43f07558045157b1bd
sobogil <sotomas232@gmail.com>
2025-07-31 18:06:50 +0900

    refactor: segment 컴포넌트 구조 정리와 리팩토링

--- daycan-front/packages/ui/src/components/Segment/Segment.tsx
+++ daycan-front/packages/ui/src/components/Segment/Segment.tsx
@@ -1,59 +1,80 @@
 import React, { PropsWithChildren, HTMLAttributes } from 'react';
 import { classNames } from "@/utils";
+import { segment, type SegmentVariants, segmentDivider } from "./Segment.css";
+import { SegmentItem } from './SegmentItem';
 
-import { segment, type SegmentVariants } from "./Segment.css";
-import { CustomWidthHeightType } from "@/utils";
-import { ChipSegment } from './ChipSegment';
-import { ItemSegment } from './ItemSegment';
+
+
 
 export type SegmentProps = PropsWithChildren<
-  Omit<HTMLAttributes<HTMLDivElement>, 'onChange'> & 
-  SegmentVariants & 
-  CustomWidthHeightType & {
+  HTMLAttributes<HTMLDivElement> & 
+  SegmentVariants & {
     options: string[];
     value: string;
-    onChange: (val: string) => void;
+    onSegmentChange: (val: string) => void;
   }
 >;
 
-/* 
-    Segment 컴포넌트입니다.
-    @param variant - Segment 스타일 변형
-    @param options - 선택 가능한 옵션들
-    @param value - 현재 선택된 값
-    @param onChange - 값 변경 핸들러
-    @param useChip - true면 Chip 컴포넌트 사용, false면 SegmentItem 컴포넌트 사용 (기본값: false)
-    @returns Segment 컴포넌트
-*/
-
 export const Segment = ({
   variant,
+  type,
   children,
   options,
-  onChange,
+  onSegmentChange,
   value,
-  useChip = false,
   ...props
 }: SegmentProps) => {
   return (
     <div
-      className={classNames(segment({ variant, useChip }))}
-      style={{ width: props.customWidth, height: props.customHeight }}
+      className={classNames(segment({ variant, type }))}
       {...props}
     >
-      {useChip ? (
-        <ChipSegment 
-          options={options}
-          value={value}
-          onChange={onChange}
-        />
-      ) : (
-        <ItemSegment 
-          options={options}
-          value={value}
-          onChange={onChange}
-        />
-      )}
+      {options.map((option: string, index: number) => {
+        const { isSelected, shouldShowDivider } = getSegmentItemState(
+          option,
+          index,
+          options,
+          value,
+          type
+        );
+
+        return (
+          <>
+            <SegmentItem 
+              key={option}
+              type={type}
+              selected={isSelected}
+              onClick={() => onSegmentChange(option)}
+            >
+              {option}
+            </SegmentItem>
+            {shouldShowDivider && <div className={classNames(segmentDivider)} />}
+          </>
+        );
+      })}
     </div>
   );
+};
+
+// 세그먼트 아이템의 상태를 계산하는 헬퍼 함수
+const getSegmentItemState = (
+  option: string,
+  index: number,
+  options: string[],
+  value: string,
+  type?: string
+) => {
+  const isSelected = value === option;
+  const isNextSelected = index < options.length - 1 && value === options[index + 1];
+  
+  // 구분선 표시 조건들을 명확하게 분리
+  const isNotLastItem = index < options.length - 1;
+  const isDefaultType = type === 'default';
+  const shouldHideDivider = isSelected || isNextSelected;
+  const shouldShowDivider = isDefaultType && isNotLastItem && !shouldHideDivider;
+
+  return {
+    isSelected,
+    shouldShowDivider,
+  };
 };
