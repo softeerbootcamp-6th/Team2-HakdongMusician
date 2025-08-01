4cc604641d2b7fe43aae34d6eb61b09af35a3a36
홍규진 <hkj0206@dgu.ac.kr>
2025-08-01 12:08:16 +0900

    feat: 공통 컴포넌트 구현 및 export

---
+++ daycan-front/apps/client/src/components/index.ts
@@ -0,0 +1,10 @@
+/**
+ * apps/client 내에서 페이지를 기준으로
+ * 두 페이지 이상인 곳에서 재사용되는 컴포넌트들만 모아둔 곳입니다.
+ * 한 페이지 내에서만 재사용되는 컴포넌트는 페이지 내의 컴포넌트에서 정의합니다.
+ * @author 홍규진
+ */
+export { HighlightingHeading } from "./HighlightingHeading";
+export { SemiCircularGaugeChart } from "./SemiCircularGaugeChart";
+export { Footer } from "./Footer/Footer";
+export { Header } from "./Header/Header";
