c1277f73bd18d5004990f6bea8d442ec932e7858
홍규진 <hkj0206@dgu.ac.kr>
2025-08-01 12:11:45 +0900

    feat: 리포트 리다이렉팅 페이지 내 커스텀 훅 구현

---
+++ daycan-front/apps/client/src/pages/to-report/hooks/useToReportHook.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const useToReportHook = () => {
+  const navigate = useNavigate();
+  const [isAnimationComplete, setIsAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAnimationComplete) {
+      const timer = setTimeout(() => {
+        navigate("/report");
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+    return () => {
+      setIsAnimationComplete(false);
+    };
+  }, [isAnimationComplete, navigate]);
+
+  const handleAnimationComplete = () => {
+    setIsAnimationComplete(true);
+    navigate("/report");
+  };
+
+  return {
+    isAnimationComplete,
+    handleAnimationComplete,
+  };
+};
