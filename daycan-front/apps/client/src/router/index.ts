b8545008fb0a19fdb0aa6273d4caabff7bf370a5
홍규진 <hkj0206@dgu.ac.kr>
2025-08-01 12:12:26 +0900

    feat: 라우팅 적용 및 의존성 추가 - recharts

---
+++ daycan-front/apps/client/src/router/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 라우터 관련 파일들을 모아둔 곳입니다.
+ * Route 는 라우팅 컴포넌트로 실질적으로 App.tsx 에서 사용됩니다.
+ * routes.tsx 는 실질적으로 각 주소에 어떤 페이지를 보여줄지 정의하는 파일입니다.
+ * @author 홍규진
+ */
+export * from "./routes";
+export * from "./Route";
