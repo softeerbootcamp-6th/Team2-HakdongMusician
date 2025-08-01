0238511c494e55cbfdce293351a32bddb6f8cf43
sobogil <sotomas232@gmail.com>
2025-08-01 17:52:45 +0900

    feat: Heading color 기능 추가

--- daycan-front/packages/ui/src/components/Heading/Heading.tsx
+++ daycan-front/packages/ui/src/components/Heading/Heading.tsx
@@ -6,9 +6,11 @@ export type HeadingProps = PropsWithChildren<
   HTMLAttributes<HTMLHeadingElement> & HeadingVariants
 >;
 
-export const Heading = ({ type, children, ...props }: HeadingProps) => {
+export const Heading = ({ type, children, color, ...props }: HeadingProps) => {
   return (
-    <h1 className={classNames(heading({ type }))} {...props}>
+    <h1 className={classNames(heading({ type }))} 
+        style={{ color: color ? color : undefined }}
+    {...props}>
       {children}
     </h1>
   );
