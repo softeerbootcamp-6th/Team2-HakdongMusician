4dcb628b2332a4d63685088f3f883212d9148881
sobogil <sotomas232@gmail.com>
2025-08-02 14:57:08 +0900

    refactor: Sidebar 컴포넌트에서 자주 사용하는 컴포넌트 MenuItemHeader 컴포넌트로 분리

---
+++ daycan-front/apps/admin/src/components/MenuItemHeader/MenuItemHeader.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+
+export const menuItemHeader = style({
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "row",
+  gap: "8px",
+});
---
+++ daycan-front/apps/admin/src/components/MenuItemHeader/MenuItemHeader.tsx
@@ -0,0 +1,44 @@
+import { Body, COLORS, Icon } from "@daycan/ui";
+import { menuItemHeader } from "./MenuItemHeader.css";
+import { type PageKey } from "../Sidebar/Sidebar";
+
+interface MenuItemHeaderProps {
+  pageKey: PageKey;
+  iconName: "record" | "report" | "elder" | "worker";
+  label: string;
+  isSelected: boolean;
+  onClick: (pageKey: PageKey) => void;
+}
+
+export const MenuItemHeader = ({
+  pageKey,
+  iconName,
+  label,
+  isSelected,
+  onClick,
+}: MenuItemHeaderProps) => {
+  const color = isSelected ? COLORS.primary[300] : COLORS.gray[500];
+
+  return (
+    <div
+      className={menuItemHeader}
+      onClick={() => onClick(pageKey)}
+    >
+      <Icon
+        name={iconName}
+        width={36}
+        height={36}
+        color={color}
+      />
+      <Body type="large" color={color}>
+        {label}
+      </Body>
+      <Icon
+        name="arrowRight"
+        width={16}
+        height={16}
+        stroke={color}
+      />
+    </div>
+  );
+};
