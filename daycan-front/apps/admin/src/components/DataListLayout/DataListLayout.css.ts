560cdc27c91b51bdece4d5707ea9854641fe4056
sobogil <sotomas232@gmail.com>
2025-08-07 17:37:45 +0900

    feat: DataListLayout 컴포넌트 제작

---
+++ daycan-front/apps/admin/src/components/DataListLayout/DataListLayout.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const memberListContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  gap: "8px",
+  boxSizing: "border-box",
+});
---
+++ daycan-front/apps/admin/src/components/DataListLayout/index.ts
@@ -0,0 +1,13 @@
+/*
+ * 종사자, 수급자의 데이터 리스트 레이아웃 컴포넌트
+ * Data라는 이름의 의미는 리스트에 들어갈 종사자 혹은 수급자 데이터를 포괄적으로 하기 위해 만들었습니다.
+ *
+ * 이 컴포넌트는 데이터 리스트의 헤더와 바디를 구성합니다.
+ * DataListHeader와 DataListRow 컴포넌트를 사용하여 리스트의 헤더와 각 행을 구성합니다.
+ *
+ * DataListHeader와 DataListRow에 맞는 데이터를 각 page내부에서 만든다음 DataListLayout에 조합하여 사용합니다.
+ *
+ * @author 소보길
+ *
+ */
+export { DataListLayout } from "./DataListLayout";
