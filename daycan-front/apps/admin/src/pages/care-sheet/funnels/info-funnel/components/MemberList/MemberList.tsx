c9ee412be25febaeaf6756d18fc651d854512f75
홍규진 <hkj0206@dgu.ac.kr>
2025-08-07 22:55:30 +0900

    feat: InfoFunnel 내 컴포넌트 구현 - MemberList

---
+++ daycan-front/apps/admin/src/pages/care-sheet/funnels/info-funnel/components/MemberList/MemberList.tsx
@@ -0,0 +1,31 @@
+import { MemberListItem } from "../MemberListItem/MemberListItem";
+import { memberListContainer } from "./MemberList.css";
+import type { Member } from "./types";
+
+interface MemberListProps {
+  members: Member[];
+  selectedMemberId?: string;
+  onMemberSelect?: (member: Member) => void;
+}
+
+export const MemberList = ({
+  members,
+  selectedMemberId,
+  onMemberSelect,
+}: MemberListProps) => {
+  return (
+    <div className={memberListContainer}>
+      {members.map((member) => (
+        <MemberListItem
+          key={member.id}
+          name={member.name}
+          birthDate={member.birthDate}
+          code={member.code}
+          profileImage={member.profileImage}
+          isSelected={selectedMemberId === member.id}
+          onClick={() => onMemberSelect?.(member)}
+        />
+      ))}
+    </div>
+  );
+};
