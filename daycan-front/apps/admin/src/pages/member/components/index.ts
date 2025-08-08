8f4cc96e50d994b95c672906a173883d1120c1bb
sobogil <sotomas232@gmail.com>
2025-08-08 10:50:11 +0900

    feat: member페이지내 컴포넌트 정의

---
+++ daycan-front/apps/admin/src/pages/member/components/index.ts
@@ -0,0 +1,5 @@
+export { MemberDataItems } from "./MemberDataItems/MemberDataItems";
+export { MemberDataItemDetail } from "./MemberDataItemDetail/MemberDataItemDetail";
+export { MemberDataListHeader } from "./MemberDataListHeader/MemberDataListHeader";
+export { MemberDetailCard } from "./MemberDetailCard/MemberDetailCard";
+export { GuardianDetailCard } from "./GuardianDetailCard/GuardianDetailCard";
