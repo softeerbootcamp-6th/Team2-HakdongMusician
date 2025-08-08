b6dbac29d09bafafa457ed1ef4a2276364db1c0d
sobogil <sotomas232@gmail.com>
2025-08-08 10:47:12 +0900

    feat: MemberDataList 컴포넌트 수정

--- daycan-front/apps/admin/src/pages/member/components/MemberDataList/MemberDataList.tsx
+++ daycan-front/apps/admin/src/pages/member/components/MemberDataList/MemberDataList.tsx
@@ -1,42 +1,12 @@
-import { DataListLayout } from "@/components/DataListLayout/DataListLayout";
-import { MEMBER_HEADERS } from "@/constants/memberList";
-import { DataListRow } from "@/components/DataListRow/DataListRow";
-import { type AdminMemberAndGuardianResponse } from "@/types";
-import { MemberDataSection } from "../MemberDataSection/MemberDataSection";
-import { DataListHeader } from "@/components/DataListHeader/DataListHeader";
+import { memberDataListContainer } from "./MemberDataList.css.ts";
+import { MemberDataListHeader } from "../MemberDataListHeader";
+import { MemberDataItems } from "../MemberDataItems/index.ts";
 
-interface MemberDataListProps {
-  memberDatas: AdminMemberAndGuardianResponse[];
-}
-
-export const MemberDataList = ({ memberDatas }: MemberDataListProps) => {
-  const handleMemberDetail = (member: AdminMemberAndGuardianResponse) => {
-    console.log("수급자 상세 정보:", member);
-  };
-
-  const dataListHeader = <DataListHeader columns={MEMBER_HEADERS} />;
-
-  const memberDataSections = memberDatas.map((memberData) => (
-    <MemberDataSection memberData={memberData} />
-  ));
-
-  const dataListRows = memberDatas.map((memberData, index) => (
-    <DataListRow
-      key={index}
-      data={memberData}
-      index={index}
-      onDetailClick={() => handleMemberDetail(memberData)}
-      dataSection={memberDataSections[index]}
-    />
-  ));
-
-  /* 
-    여기서 DataListLayout 컴포넌트를 사용하여 리턴
-  */
+export const MemberDataList = () => {
   return (
-    <DataListLayout
-      dataListHeader={dataListHeader}
-      dataListRows={dataListRows}
-    />
+    <div className={memberDataListContainer}>
+      <MemberDataListHeader />
+      <MemberDataItems />
+    </div>
   );
 };
--- daycan-front/apps/admin/src/pages/member/components/MemberDataList/index.ts
+++ daycan-front/apps/admin/src/pages/member/components/MemberDataList/index.ts
@@ -1,7 +1,1 @@
-/*
- * 종사자, 수급자 관리 페이지에서 공통으로 사용되는 MemberDataList 컴포넌트입니다.
- * DataListLayout을 구현한다음 page에서 사용합니다.
- * @author 소보길
- */
-
 export { MemberDataList } from "./MemberDataList";
