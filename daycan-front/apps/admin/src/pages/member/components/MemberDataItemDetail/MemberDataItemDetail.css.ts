e8fb764381abbb6751fa525fcfbe2dcc50b32dd4
sobogil <sotomas232@gmail.com>
2025-08-08 10:48:19 +0900

    feat: MemberDataItemDetail 컴포넌트 제작

---
+++ daycan-front/apps/admin/src/pages/member/components/MemberDataItemDetail/MemberDataItemDetail.css.ts
@@ -0,0 +1,55 @@
+import { style } from "@vanilla-extract/css";
+import { COLORS } from "@daycan/ui";
+
+export const dataSectionContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  width: "1084px",
+  boxSizing: "border-box",
+  borderTop: `1px solid ${COLORS.gray[200]}`,
+});
+
+export const dataSectionContent = style({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  gap: "16px",
+  paddingLeft: "33px",
+  paddingRight: "16px",
+  paddingTop: "16px",
+  paddingBottom: "24px",
+  borderRadius: "0 0 8px 8px",
+  backgroundColor: "#FFFFFF",
+  boxShadow: `0px 0px 4px rgba(0, 0, 0, 0.05)`,
+});
+
+export const dataSectionButtonContainer = style({
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "8px 16px",
+  borderRadius: "4px",
+  border: "none",
+  width: "100%",
+  height: "100%",
+  minHeight: "250px",
+});
+
+export const dataSectionTopButton = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "8px",
+  width: "100%",
+});
+
+export const dataSectionBottomButton = style({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "flex-end",
+  marginLeft: "auto",
+  gap: "8px",
+});
---
+++ daycan-front/apps/admin/src/pages/member/components/MemberDataItemDetail/MemberDataItemDetail.tsx
@@ -0,0 +1,56 @@
+import { Button, COLORS, Icon } from "@daycan/ui";
+import {
+  dataSectionContainer,
+  dataSectionContent,
+  dataSectionButtonContainer,
+  dataSectionTopButton,
+  dataSectionBottomButton,
+} from "./MemberDataItemDetail.css";
+
+interface MemberDataItemDetailProps {
+  detailCard?: React.ReactNode;
+}
+
+export const MemberDataItemDetail = ({
+  detailCard,
+}: MemberDataItemDetailProps) => {
+  return (
+    <div className={dataSectionContainer}>
+      <div className={dataSectionContent}>
+        {detailCard}
+
+        <div className={dataSectionButtonContainer}>
+          <div className={dataSectionTopButton}>
+            <Button
+              size="fullWidth"
+              style={{ backgroundColor: COLORS.gray[600], color: COLORS.white }}
+            >
+              기록지 및 리포트 내역
+              <Icon name="arrowRight" stroke="currentColor" />
+            </Button>
+          </div>
+          <div className={dataSectionBottomButton}>
+            <Button
+              size="small"
+              style={{
+                backgroundColor: COLORS.green[500],
+                color: COLORS.green[200],
+                width: "58px",
+                height: "32px",
+              }}
+            >
+              수정
+            </Button>
+            <Button
+              variant="error"
+              size="small"
+              style={{ width: "58px", height: "32px" }}
+            >
+              삭제
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
