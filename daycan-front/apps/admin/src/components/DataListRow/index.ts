5af1999f13b27631cc24e1a2aa4e369a60b65571
sobogil <sotomas232@gmail.com>
2025-08-08 10:41:16 +0900

    delete: DataListRow컴포넌트 삭제

--- daycan-front/apps/admin/src/components/DataListRow/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * 종사자, 수급자 관리 페이지에서 공통으로 사용되는 DataListRow 컴포넌트입니다.
- * 각 행의 데이터를 표시하며, 상세보기 기능을 포함합니다.
- * 각 행 한줄이라고 생각하시면 됩니다.
- * @author 소보길
- */
-
-export { DataListRow } from "./DataListRow";
