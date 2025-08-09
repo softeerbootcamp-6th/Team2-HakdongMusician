ff396762246d4ef2bcdbb610e98d771a9ca62860
홍규진 <hkj0206@dgu.ac.kr>
2025-08-09 23:13:00 +0900

    feat: diagnosis-funnel 내 컴포넌트 구현 - RowCheckBox

---
+++ daycan-front/apps/admin/src/pages/care-sheet/funnels/diagnosis-funnel/components/RowCheckBox/RowCheckBox.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const rowCheckBoxContainer = style({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+});
