1c5031f810d4bb2ff53bf7041f935e4f69b05d7f
genius00hwan <qnfn120@gmail.com>
2025-08-11 18:22:52 +0900

    hotfix: fix compile error

--- daycan-back/api/src/main/java/com/daycan/api/dto/member/request/ReportQueryParameters.java
+++ daycan-back/api/src/main/java/com/daycan/api/dto/member/request/ReportQueryParameters.java
@@ -4,12 +4,10 @@ package com.daycan.api.dto.member.request;
 import com.daycan.api.dto.entry.document.report.ReportStatus;
 import com.daycan.domain.enums.SortDirection;
 import com.daycan.api.dto.entry.document.DayFilter;
-import com.daycan.api.dto.validation.annotations.ReportStatusesOnly;
 import java.util.List;
 
 public record ReportQueryParameters(
     DayFilter dayFilter,
-    @ReportStatusesOnly
     List<ReportStatus> statuses,
     String memberNameLike,
     SortDirection order
