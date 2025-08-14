3245bb28e1fdcfb238dba704a1c770e110f9f3ab
홍규진 <hkj0206@dgu.ac.kr>
2025-08-14 11:54:04 +0900

    feat: 선택된 기록지 관리를 위한 careSheetAtom 추가

---
+++ daycan-front/apps/admin/src/pages/care-sheet/atoms/index.ts
@@ -0,0 +1,5 @@
+/**
+ * 기록지 페이지에서, 어떤 기록지들이 선택되었는지를 관리하는 atom입니다.
+ * 해당 atom은 출석 인원과 결석 인원 두 개의 리스트에서 사용되며 API 호출 시 해당 atom을 통해 선택된 기록지들을 전달합니다.
+ */
+export { checkedCareSheetIdsAtom } from "./careSheetAtom";
