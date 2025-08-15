7b9b01e088419bfe4e3151e9d1a70f138ea7d5d1
홍규진 <hkj0206@dgu.ac.kr>
2025-08-15 20:26:06 +0900

    feat: api 패키지 빌드 세팅 및 export 수정

--- daycan-front/packages/api/vite.config.ts
+++ daycan-front/packages/api/vite.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from "vite";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "./index.ts"),
       name: "DaycanAPI",
       fileName: (format) => `daycan-api.${format}.js`,
+      formats: ["es", "umd"],
     },
     rollupOptions: {
-      external: [],
+      external: ["@tanstack/react-query", "axios"],
       output: {
-        globals: {},
+        globals: {
+          "@tanstack/react-query": "TanStackQuery",
+          axios: "axios",
+        },
       },
     },
   },
