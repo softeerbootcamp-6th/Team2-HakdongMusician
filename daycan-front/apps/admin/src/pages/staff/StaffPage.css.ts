49b49d76d8f1b149c2e0d42c8211ad98826ca7a8
sobogil <sotomas232@gmail.com>
2025-08-15 21:42:20 +0900

    feat: Staff 페이지 제작 & 더미파일 생성

---
+++ daycan-front/apps/admin/src/pages/staff/StaffPage.css.ts
@@ -0,0 +1,19 @@
+import { COLORS } from "@daycan/ui";
+import { style } from "@vanilla-extract/css";
+
+export const staffPageContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  width: "1084px",
+  boxSizing: "border-box",
+});
+
+export const staffButton = style({
+  width: "184px",
+});
+
+export const staffSearchContainer = style({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+});
---
+++ daycan-front/apps/admin/src/pages/staff/index.ts
@@ -0,0 +1,6 @@
+/*
+ * 종사자 관리 페이지 컴포넌트
+ * @author 소보길
+ */
+
+export * from "./StaffPage";
