5e5ad175bfffd54ede4283646eb5a311f2ede770
sobogil <sotomas232@gmail.com>
2025-08-15 21:47:58 +0900

    feat: StaffRegisterLayout 컴포넌트 제작

---
+++ daycan-front/apps/admin/src/pages/staff-register/components/StaffRegisterLayout/index.ts
@@ -0,0 +1,9 @@
+/*
+ * StaffRegisterLayout은 종사자 등록 페이지의 레이아웃을 관리하는 컴포넌트입니다.
+ * StaffInfoSection을 children으로 받아서 관리합니다.
+ * 현재는 종사자 정보만 children으로 받고 있고 후에 추가되는
+ * 정보를 관리하려면 StaffInfoSection을 하나 더 만들어서 children에 넣어주면 됩니다.
+ * @author 소보길
+ */
+
+export { StaffRegisterLayout } from "./StaffRegisterLayout";
