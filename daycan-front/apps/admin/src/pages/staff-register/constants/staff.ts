a3c59081514eabf7cd6f219f7203e89a90a256fe
sobogil <sotomas232@gmail.com>
2025-08-15 21:53:02 +0900

    feat: Staff-Register 페이지 내 더미 파일, type, util 제작

---
+++ daycan-front/apps/admin/src/pages/staff-register/constants/staff.ts
@@ -0,0 +1,10 @@
+export type StaffListResponse = {
+  staffId: number;
+  centerId: string;
+  name: string;
+  gender: string;
+  staffRole: string;
+  birthDate: string;
+  phoneNumber: string;
+  avatarUrl: string;
+};
