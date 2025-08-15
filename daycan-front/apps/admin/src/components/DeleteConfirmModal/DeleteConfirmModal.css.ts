9ed21256bedc80cac600ed86ab6f7fdc8dd90278
sobogil <sotomas232@gmail.com>
2025-08-15 22:03:59 +0900

    refactor: MemberDeleteConfirm -> DeleteConfirm 컴포넌트로 변경과 admin내 공통 컴포넌트로 변경

--- daycan-front/apps/admin/src/components/DeleteConfirmModal/DeleteConfirmModal.css.ts
+++ daycan-front/apps/admin/src/components/DeleteConfirmModal/DeleteConfirmModal.css.ts
@@ -1,6 +1,6 @@
 import { style } from "@vanilla-extract/css";
 
-export const memberDeleteConfirmModalContainer = style({
+export const deleteConfirmModalContainer = style({
   width: "700px",
   padding: "24px",
   height: "181px",
@@ -9,21 +9,21 @@ export const memberDeleteConfirmModalContainer = style({
   justifyContent: "space-between",
 });
 
-export const memberDeleteConfirmModalTitle = style({
+export const deleteConfirmModalTitle = style({
   display: "flex",
   flexDirection: "column",
   gap: "8px",
   marginBottom: "12px",
 });
 
-export const memberDeleteConfirmModalContent = style({
+export const deleteConfirmModalContent = style({
   display: "flex",
   flexDirection: "column",
   gap: "24px",
   paddingTop: "10px",
 });
 
-export const memberDeleteConfirmModalButton = style({
+export const deleteConfirmModalButton = style({
   display: "flex",
   gap: "8px",
 
--- daycan-front/apps/admin/src/components/DeleteConfirmModal/DeleteConfirmModal.tsx
+++ daycan-front/apps/admin/src/components/DeleteConfirmModal/DeleteConfirmModal.tsx
@@ -1,35 +1,37 @@
 import { Body, Button, Heading, Modal } from "@daycan/ui";
 import {
-  memberDeleteConfirmModalContainer,
-  memberDeleteConfirmModalTitle,
-  memberDeleteConfirmModalContent,
-  memberDeleteConfirmModalButton,
-} from "./MemberDeleteConfirmModal.css";
+  deleteConfirmModalContainer,
+  deleteConfirmModalTitle,
+  deleteConfirmModalContent,
+  deleteConfirmModalButton,
+} from "./DeleteConfirmModal.css";
 
-interface MemberDeleteConfirmModalProps {
+interface DeleteConfirmModalProps {
   isOpen: boolean;
   onClose: () => void;
   onDeleteConfirm: () => void;
+  unitName: string;
 }
 
-export const MemberDeleteConfirmModal = ({
+export const DeleteConfirmModal = ({
   isOpen,
   onClose,
   onDeleteConfirm,
-}: MemberDeleteConfirmModalProps) => {
+  unitName,
+}: DeleteConfirmModalProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className={memberDeleteConfirmModalContainer}>
-        <div className={memberDeleteConfirmModalTitle}>
+      <div className={deleteConfirmModalContainer}>
+        <div className={deleteConfirmModalTitle}>
           <Heading type="xsmall" weight={600}>
-            정말 김노인 님의 정보를 삭제하시겠어요?
+            정말 {unitName}님의 정보를 삭제하시겠어요?
           </Heading>
           <Body type="medium" weight={400}>
             삭제하면 모든 기록이 사라지며 복구할 수 없어요.
           </Body>
         </div>
-        <div className={memberDeleteConfirmModalContent}>
-          <div className={memberDeleteConfirmModalButton}>
+        <div className={deleteConfirmModalContent}>
+          <div className={deleteConfirmModalButton}>
             <Button variant="unEmphasized" onClick={onClose} size="fullWidth">
               닫기
             </Button>
