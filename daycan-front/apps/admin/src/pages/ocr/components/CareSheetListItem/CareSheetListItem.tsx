467b7d073185739aa48212b1247967581a6909b1
홍규진 <hkj0206@dgu.ac.kr>
2025-08-18 01:22:39 +0900

    feat: OCR 페이지 퍼블리싱 및 더미데이터 주입

---
+++ daycan-front/apps/admin/src/pages/ocr/components/CareSheetListItem/CareSheetListItem.tsx
@@ -0,0 +1,43 @@
+import type { TCareSheetListItem } from "@/services/careSheet/types";
+import {
+  careSheetListItemContainer,
+  careSheetListItemProfile,
+  careSheetListItemInfoContainer,
+  careSheetListItemStatusContainer,
+} from "./CareSheetListItem.css";
+import { Body, COLORS, Icon } from "@daycan/ui";
+
+interface CareSheetListItemProps {
+  careSheet: TCareSheetListItem;
+}
+
+export const CareSheetListItem = ({ careSheet }: CareSheetListItemProps) => {
+  return (
+    <div className={careSheetListItemContainer}>
+      <img
+        src={careSheet.memberMeta.avatarUrl}
+        alt="프로필"
+        className={careSheetListItemProfile}
+      />
+      <div className={careSheetListItemInfoContainer}>
+        <Body type="medium" weight={500} color={COLORS.gray[600]}>
+          {careSheet.memberMeta.name}
+        </Body>
+      </div>
+      <div>
+        <div className={careSheetListItemStatusContainer}>
+          <Body type="small" weight={500} color={COLORS.gray[600]}>
+            기록지 보러 가기
+          </Body>
+          <Icon
+            name={"chevronRight"}
+            width={20}
+            height={20}
+            color={COLORS.gray[200]}
+            stroke={COLORS.white}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ daycan-front/apps/admin/src/pages/ocr/components/CareSheetListItem/index.ts
@@ -0,0 +1,6 @@
+/**
+ * 기록지 리스트 아이템 컴포넌트입니다.
+ * OCR 페이지에서 사용됩니다.
+ * @author 홍규진
+ */
+export * from "./CareSheetListItem";
