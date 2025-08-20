72b07897535cc272f80dd059aad96721f41940e8
sobogil <sotomas232@gmail.com>
2025-08-21 00:16:40 +0900

    feat: MemberRegisterLayout 컴포넌트 생성

---
+++ daycan-front/apps/admin/src/pages/member-register/components/MemberRegisterLayout/index.ts
@@ -0,0 +1,9 @@
+/*
+ * MemberRegisterLayout은 수급자 등록 페이지의 레이아웃을 관리하는 컴포넌트입니다.
+ * MemberInfoSection, GuardianInfoSection, ReportConsentSection을 children으로 받아서 관리합니다.
+ *
+ * 정보를 관리하려면 하나 더 만들어서 children에 넣어주면 됩니다.
+ * @author 소보길
+ */
+
+export { MemberRegisterLayout } from "./MemberRegisterLayout";
